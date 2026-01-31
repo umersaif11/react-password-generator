ed18bc86478635b8a0468ba29532bf1d35f621b5
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-01-31 16:31:53 +0500

    Make the function that will generate password everytime you slide length, click characters or numbers

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,21 @@ function App() {
   const [numAllowed, setNumAllowed] = useState(false)
   const [charAllowed, setcharAllowed] = useState(false)
   const [password, setPassword] = useState("")
-  const passwordGenerator = useCallback(fn, [
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(charAllowed) string += "!@#$%^&*()_-+=[]{}~`"
+    if(numAllowed) string += "0123456789"
+
+    for(let i = 0; i <= length; i++) {
+      let char = Math.floor(
+        Math.random(string.length) + 1
+      )
+      pass += string.charAt(char)
+    }
+    setPassword(pass)
+
+  }, [
     length, numAllowed, charAllowed, setPassword
   ])
   return (
