415024945d9483d1087e6cc3d8d628dcdd2a15ca
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-01-31 18:17:38 +0500

    Add the inputs checkboxes for numbers and characters

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <>
       <div className="w-full max-w-full mx-auto 
-      bg-gray-700 shadow-md
+      bg-gray-700 text-orange-500 shadow-md
       px-4 py-3 rounded-lg">
         <h2 className="text-white text-center">
           Password Generator
@@ -38,8 +38,44 @@ function App() {
           type="text"
           value={password}
           placeholder="Password"
-          className="outline-none px-3 py-1 text-gray-700"
+          className="outline-none w-full px-3 py-1 text-gray-700"
+          readOnly
           />
+          <button className="bg-blue-700! text-white
+          shrink-0 px-3! py-0.5!">
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm justify-around">
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer accent-blue-700"
+            onChange={(e) => {setLength(e.target.value)}}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="checkbox"
+            defaultChecked={numAllowed}
+            className="cursor-pointer accent-blue-700"
+            onChange={() => {setNumAllowed(prev => !prev)}}
+            />
+            <label>Numbers</label>
+          </div>
+           <div className="flex items-center gap-x-1">
+            <input 
+            type="checkbox"
+            defaultChecked={charAllowed}
+            className="cursor-pointer accent-blue-700"
+            onChange={() => {setcharAllowed(prev => !prev)}}
+            />
+            <label>Characters</label>
+          </div>
         </div>
       </div>
     </>
