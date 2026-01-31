3343a06e5662f13b25784bef30e0cd572991ff66
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-01-31 17:24:02 +0500

    Make the div holding h2 and input

--- src/App.jsx
+++ src/App.jsx
@@ -26,9 +26,22 @@ function App() {
   ])
   return (
     <>
-      <h1 className='text-4xl text-center'>
-        Password Generator
-      </h1>
+      <div className="w-full max-w-full mx-auto 
+      bg-gray-700 shadow-md
+      px-4 py-3 rounded-lg">
+        <h2 className="text-white text-center">
+          Password Generator
+        </h2>
+        <div className="flex shadow rounded-lg
+        overflow-hidden mb-4 mt-5 bg-white">
+          <input 
+          type="text"
+          value={password}
+          placeholder="Password"
+          className="outline-none px-3 py-1 text-gray-700"
+          />
+        </div>
+      </div>
     </>
   )
 }
